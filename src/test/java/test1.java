d182ef6bd331bfaae0a975e0c38d6179c5e24231
LainQa <lainqa63@gmail.com>
2021-11-06 18:14:32 +0400

    Initial commit

---
+++ src/test/java/test1.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+
+public class test1 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize= "2560x1440";
+    }
+
+    @Test
+    void wiki1(){
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link > button").click();
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        $("#wiki-wrapper").shouldHave(text("com.codeborne.selenide.junit.SoftAsserts"));
+
+    }
+
+
+
+
+    }
+
+
+
